79ba63ebca14dd855368d24cd6730f685890be77
Serhii <peskosergij@gmail.com>
2023-03-07 22:27:16 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnDestroy } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'internship';
+export class AppComponent implements OnDestroy {
+  file!: File | null;
+  fileName = '';
+  errorMessage = '';
+  email = '';
+  fileSent = true;
+  sub!: Subscription;
+  acceptableFileType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+  constructor(private matSnackBar: MatSnackBar,
+  private _httpClient: HttpClient) {}
+
+  sendFile() {
+    const emailReg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if(!emailReg.test(this.email)) {
+      this.matSnackBar.open("Enter a valid email address", "Ok");
+    }
+    else if(!this.file) {
+      this.matSnackBar.open("Choose a valid file", "Ok");
+    }
+    else
+    {
+      const domain = this.email.split('.');
+      if(domain[domain.length - 1] == "ru" || domain[domain.length - 1] == "rus") {
+        this.matSnackBar.open("Glory to Ukraine", "Ok");
+      } else {
+        this.fileSent = false;
+        const formData = new FormData();
+        formData.append(this.file.name, this.file);
+        formData.append('email', this.email);
+        this.sub = this._httpClient.post('https://internshipspeshko.azurewebsites.net/api/file', formData)
+          .subscribe(resp => {
+            this.fileSent = true;
+            this.matSnackBar.open('File has been sent successfuly', 'Ok');
+          }, error => {
+            this.fileSent = true;
+            console.log(error);
+          })
+      }
+    }
+  }
+
+  onFileSelected(event: any) {
+    this.file = event.target.files[0];
+    if(this.file) {
+      if(this.file.type !== this.acceptableFileType) {
+        this.errorMessage = "Invalid file type. Acceptable file types: .docx";
+        this.fileName = '';
+        this.file = null;
+      } else {
+        this.fileName = this.file.name;
+        this.errorMessage = '';
+      }
+    } 
+  }
+
+  ngOnDestroy(): void {
+    if(this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    FormsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
